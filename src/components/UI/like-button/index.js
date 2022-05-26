6fefa2b90a2494c2dfb8c36cdd8664318643c84f
Nikolaj Gromkov <nikolajgromkov@gmail.com>
2022-05-26 23:07:23 +0800

    likes finish

--- src/components/UI/like-button/index.js
+++ src/components/UI/like-button/index.js
@@ -1,1 +1,12 @@
 import './like-button.scss'
+
+const allLikeButtons = document.querySelectorAll('.like-button');
+
+allLikeButtons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const target = e.currentTarget;
+    const likesCount = target.querySelector('.like-button__counter');
+    likesCount.innerText = parseInt(likesCount.innerText) + 1;
+    target.classList.add('like-button--active');
+  })
+})
