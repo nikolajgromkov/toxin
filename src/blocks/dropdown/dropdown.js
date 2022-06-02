8f92ab958b38aef7498fa24e2e7c17aa110e2a11
Nikolaj Gromkov <nikolajgromkov@gmail.com>
2022-06-02 22:57:19 +0800

    start change dropdown text

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -19,9 +19,9 @@ const pluralize = (num, declensions) => {
 
 ready(function() {
 
-  console.log(pluralize(13, ["гость", "гостя", "гостей"]));
 
-  
+  const allControls = document.querySelectorAll(".dropdown__control")
+  const allCounters = document.querySelectorAll(".counter");
 
   const getNumberInLimits = (input, action) => {
     const result = Number(input.value) + action;
@@ -45,7 +45,17 @@ ready(function() {
   }
 
   const handleChange = (e) => {
-    const counterInput = e.currentTarget;
+
+    let resultText = '';
+
+    Array.prototype.map.call(allCounters, (counter) => {
+      const input = counter.querySelector(".counter__input");
+      resultText += input.value + " " + pluralize(input.value, input.dataset.declensions);
+      console.log(input.dataset.declensions);
+    })
+
+    document.querySelector('.dropdown__control span').innerHTML =  resultText;
+
   }
 
   const handleDropdown = (e) => {
@@ -53,17 +63,15 @@ ready(function() {
     dropdownBody.classList.toggle("show")
   }
 
-  const allControls = document.querySelectorAll(".dropdown__control")
-  const allCounters = document.querySelectorAll(".counter");
+ 
   Array.prototype.map.call(allCounters, (counter, index) => {
     counter.querySelector(".counter__minus").addEventListener('click', handleClick);
     counter.querySelector(".counter__plus").addEventListener('click', handleClick);
-    counter.querySelector(".counter__input").addEventListener('chnage', handleChange);
+    counter.querySelector(".counter__input").addEventListener('change', handleChange);
   })
 
   Array.prototype.map.call(allControls, (control, index) => {
     control.addEventListener('click', handleDropdown);
-    
   })
   
   
